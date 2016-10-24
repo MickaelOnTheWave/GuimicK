d781869249a76333d99c9cf0eb946180b6b0d3e7
Mickaël da Cruz Guimarães <mickael.dacruz@gmail.com>
2016-10-24 12:58:30 -0200

    Created Git job test suite structure.

--- TaskTests/TaskTests.pro
+++ TaskTests/TaskTests.pro
@@ -12,8 +12,38 @@ TARGET = tst_taskteststest
 CONFIG   += console
 CONFIG   -= app_bundle
 
+QMAKE_CXXFLAGS += -std=c++11
+
 TEMPLATE = app
 
 
 SOURCES += tst_taskteststest.cpp
 DEFINES += SRCDIR=\\\"$$PWD/\\\"
+
+CONFIG(debug, debug|release) {
+	BUILD_TYPE = Debug
+}
+else {
+	BUILD_TYPE = Release
+}
+
+# linking TaskLib
+TASK_LIB_PATH = /home/mickael/Prog/TaskManager/bin/$$BUILD_TYPE/TaskLib/
+TASK_INCLUDE_PATH = /home/mickael/Prog/TaskManager/TaskLib/
+LIBS += -L$$TASK_LIB_PATH -lTaskLib
+INCLUDEPATH += $$TASK_INCLUDE_PATH
+DEPENDPATH += $$TASK_INCLUDE_PATH
+
+# linking ParsersLib
+PARSERS_LIB_PATH = /home/mickael/Prog/TaskManager/bin/$$BUILD_TYPE/ReportParsers/ParsersLib/
+PARSERS_INCLUDE_PATH = /home/mickael/Prog/TaskManager/ReportParsers/ParsersLib/
+unix:!macx: LIBS += -L$$PARSERS_LIB_PATH -lParsersLib
+INCLUDEPATH += $$PARSERS_INCLUDE_PATH
+DEPENDPATH += $$PARSERS_INCLUDE_PATH
+
+# linking ToolsLib
+TOOLS_LIB_PATH = /home/mickael/Prog/Tools/bin/$$BUILD_TYPE/ToolsLib
+TOOLS_INCLUDE_PATH = /home/mickael/Prog/Tools/ToolsLib/
+LIBS += -L$$TOOLS_LIB_PATH -lToolsLib
+INCLUDEPATH += $$TOOLS_INCLUDE_PATH
+DEPENDPATH += $$TOOLS_INCLUDE_PATH
--- TaskTests/tst_taskteststest.cpp
+++ TaskTests/tst_taskteststest.cpp
@@ -1,26 +1,109 @@
 #include <QString>
 #include <QtTest>
 
-class TaskTestsTest : public QObject
+#include <filetools.h>
+#include <jobstatus.h>
+
+class GitJobTest : public QObject
 {
     Q_OBJECT
 
 public:
-    TaskTestsTest();
+    GitJobTest();
 
 private Q_SLOTS:
-    void testCase1();
+    void testCreate_NoSource();
+    void testCreate_InvalidSource();
+    void testCreate_InvalidDestination();
+    void testCreate_ExistingDestination();
+    void testCreate_AllValid();
+
+    void testUpdate_Added();
+    void testUpdate_Removed();
+    void testUpdate_Changed();
+    void testUpdate_MixedChanges();
+
+private:
+    void CreateGitBackup(const QString& source, const QString& destination);
+
+    const std::string sourceRepository = "GitRepository";
+    const std::string destinationRepository = "GitDestRepository";
+
+    JobStatus* currentStatus;
 };
 
-TaskTestsTest::TaskTestsTest()
+GitJobTest::GitJobTest()
 {
 }
 
-void TaskTestsTest::testCase1()
+// To force GCC to include definition into object file, so that it is possible
+// to use them externally.
+const int JobStatus::NOT_EXECUTED;
+//const int JobStatus::OK;
+//const int JobStatus::OK_WITH_WARNINGS;
+//const int JobStatus::ERROR;
+
+void GitJobTest::testCreate_NoSource()
 {
-    QVERIFY2(true, "Failure");
+    const QString destination = "destNonExistentPath";
+
+    CreateGitBackup("nonexistentPath", destination);
+
+    QCOMPARE(currentStatus->GetCode(), JobStatus::ERROR);
+    QCOMPARE(currentStatus->GetDescription().c_str(), "Source repository does not exist");
+
+    std::vector<std::string> reportFiles;
+    currentStatus->GetFilenames(reportFiles);
+    QCOMPARE(reportFiles.size(), 0ul);
+
+    QCOMPARE(FileTools::FolderExists(destination.toStdString()), false);
 }
 
-QTEST_APPLESS_MAIN(TaskTestsTest)
+void GitJobTest::testCreate_InvalidSource()
+{
+    QFAIL("Not implemented yet");
+}
+
+void GitJobTest::testCreate_InvalidDestination()
+{
+    QFAIL("Not implemented yet");
+}
+
+void GitJobTest::testCreate_ExistingDestination()
+{
+    QFAIL("Not implemented yet");
+}
+
+void GitJobTest::testCreate_AllValid()
+{
+    QFAIL("Not implemented yet");
+}
+
+void GitJobTest::testUpdate_Added()
+{
+    QFAIL("Not implemented yet");
+}
+
+void GitJobTest::testUpdate_Removed()
+{
+    QFAIL("Not implemented yet");
+}
+
+void GitJobTest::testUpdate_Changed()
+{
+    QFAIL("Not implemented yet");
+}
+
+void GitJobTest::testUpdate_MixedChanges()
+{
+    QFAIL("Not implemented yet");
+}
+
+void GitJobTest::CreateGitBackup(const QString &source, const QString &destination)
+{
+    // TODO : Run job, get its status and its report data.
+}
+
+QTEST_APPLESS_MAIN(GitJobTest)
 
 #include "tst_taskteststest.moc"
