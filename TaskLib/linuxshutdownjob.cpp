ff799b1d5c2f92132986d813bf97cab467d3cbd7
Mickaël da Cruz Guimarães <mickael.dacruz@gmail.com>
2017-01-19 19:38:38 -0200

    Updated Shutdown job to check when computer goes offline.

--- TaskLib/linuxshutdownjob.cpp
+++ TaskLib/linuxshutdownjob.cpp
@@ -1,6 +1,68 @@
 #include "linuxshutdownjob.h"
 
+#include <unistd.h>
+#include "tools.h"
+
+static const int DEFAULT_TIMEOUT = 120;
+
 LinuxShutdownJob::LinuxShutdownJob()
-    : SshConsoleJob("Shutdown", "shutdown", "-h 1")
+    : computer(""), jobTimeoutInSeconds(DEFAULT_TIMEOUT)
 {
+    shutdownJob = new SshConsoleJob("", "shutdown", "-h now");
+}
+
+LinuxShutdownJob::~LinuxShutdownJob()
+{
+    delete shutdownJob;
+}
+
+std::string LinuxShutdownJob::GetName()
+{
+    return "Shutdown";
+}
+
+AbstractJob *LinuxShutdownJob::Clone()
+{
+    return new LinuxShutdownJob();
+}
+
+bool LinuxShutdownJob::InitializeFromClient(Client *client)
+{
+    computer = client->GetProperty("ip");
+    bool jobInitialized = shutdownJob->InitializeFromClient(client);
+    return (jobInitialized && computer != "");
+}
+
+bool LinuxShutdownJob::IsInitialized()
+{
+    return shutdownJob->IsInitialized() && computer != "";
+}
+
+JobStatus *LinuxShutdownJob::Run()
+{
+    JobStatus* status = shutdownJob->Run();
+    if (status->GetCode() == JobStatus::ERROR)
+        return status;
+
+    int secondsToShutdown = WaitForComputerToGoDown();
+
+    if (secondsToShutdown > jobTimeoutInSeconds)
+    {
+        status->SetCode(JobStatus::ERROR);
+        status->SetDescription("Machine still running");
+    }
+
+    return status;
+}
+
+int LinuxShutdownJob::WaitForComputerToGoDown() const
+{
+    int secondsCounter = 0;
+    while (Tools::IsComputerAlive(computer) && secondsCounter < jobTimeoutInSeconds)
+    {
+        sleep(1);
+        ++secondsCounter;
+    }
+
+    return secondsCounter;
 }
--- TaskLib/linuxshutdownjob.h
+++ TaskLib/linuxshutdownjob.h
@@ -1,12 +1,34 @@
 #ifndef LINUXSHUTDOWNJOB_H
 #define LINUXSHUTDOWNJOB_H
 
+#include "abstractjob.h"
 #include "sshconsolejob.h"
 
-class LinuxShutdownJob : public SshConsoleJob
+class LinuxShutdownJob : public AbstractJob
 {
 public:
     LinuxShutdownJob();
+    virtual ~LinuxShutdownJob();
+
+    virtual std::string GetName();
+
+    virtual AbstractJob *Clone();
+
+    virtual bool InitializeFromClient(Client* client);
+
+    virtual bool IsInitialized(void);
+
+    virtual JobStatus* Run();
+
+private:
+    /**
+     * @return number of seconds elapsed until computer is down
+     */
+    int WaitForComputerToGoDown() const;
+
+    SshConsoleJob* shutdownJob;
+    std::string computer;
+    int jobTimeoutInSeconds;
 };
 
 #endif // LINUXSHUTDOWNJOB_H
