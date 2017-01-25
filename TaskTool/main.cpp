e7c600b20916627094dcaca708686bf955aff99d
Mickaël da Cruz Guimarães <mickael.dacruz@gmail.com>
2017-01-25 17:37:52 -0200

    Updated version number

--- TaskTool/main.cpp
+++ TaskTool/main.cpp
@@ -23,7 +23,7 @@
 
 using namespace std;
 
-static const string PROGRAM_VERSION = "0.51";
+static const string PROGRAM_VERSION = "0.52";
 static const string DEFAULT_CONFIGURATION_FILE = "configuration.txt";
 
 void ShowErrors(list<string> &errorMessages);
