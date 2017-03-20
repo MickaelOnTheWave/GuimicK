6ac73714866bc363d24c00e23824cb29899f3265
Mickaël da Cruz Guimarães <mickael.dacruz@gmail.com>
2017-03-20 20:39:04 -0300

    Updated worklog

--- TaskTestLib/tasktestlib.cpp
+++ TaskTestLib/tasktestlib.cpp
@@ -4,6 +4,7 @@
 
 void GetTaskLibTests(std::vector<QObject*>& tests)
 {
+    // TODO : if necessary, add data path option as for Parsers test suites.
     tests.push_back(new RsnapshotErrorAnalyzerTest());
     tests.push_back(new RsnapshotJobTest());
     tests.push_back(new GitJobTest());
