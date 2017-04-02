2f7149da7dd565b11fd69c01b11d5964d735224b
Mickaël da Cruz Guimarães <mickael.dacruz@gmail.com>
2017-04-02 15:15:23 -0300

    Updated TaskFeature tests

--- TaskTests/main.cpp
+++ TaskTests/main.cpp
@@ -9,12 +9,12 @@ int main(int argc, char* argv[])
 {
     //QObject* testSuite = new RsnapshotJobTest();
     //QObject* testSuite = new RsnapshotErrorAnalyzerTest();
-    //QObject* testSuite = new TaskFeatureTest();
+    QObject* testSuite = new TaskFeatureTest();
     //QObject* testSuite = new ConsoleJobTest();
     //QObject* testSuite = new GitJobTest();
     //QObject* testSuite = new UserConsoleJobTest();
     //QObject* testSuite = new SshConsoleJobTest();
-    QObject* testSuite = new ConfigurationParserTest("/home/mickael/Prog/Tools/ToolsTestLib/data/");
+    //QObject* testSuite = new ConfigurationParserTest("/home/mickael/Prog/Tools/ToolsTestLib/data/");
     //QObject* testSuite = new FileToolsTest("/home/mickael/Prog/Tools/ToolsTestLib/data/");
     //QObject* testSuite = new ClientWorkManagerTest();
     //QObject* testSuite = new ConfigurationTest("/home/mickael/Prog/TaskManager/TaskTestLib/data/");
