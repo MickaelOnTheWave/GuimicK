6eea41d762c173f4d97808952ad57c9cf1d25354
Mickaël da Cruz Guimarães <mickael.dacruz@gmail.com>
2017-04-30 16:56:48 -0300

    Automated backup

---
+++ TaskLib/gitfsbackupjobconfiguration.cpp
@@ -0,0 +1,5 @@
+#include "gitfsbackupjobconfiguration.h"
+
+GitFsBackupJobConfiguration::GitFsBackupJobConfiguration()
+{
+}
---
+++ TaskLib/gitfsbackupjobconfiguration.h
@@ -0,0 +1,10 @@
+#ifndef GITFSBACKUPJOBCONFIGURATION_H
+#define GITFSBACKUPJOBCONFIGURATION_H
+
+class GitFsBackupJobConfiguration : public AbstractJobConfiguration
+{
+public:
+    GitFsBackupJobConfiguration();
+};
+
+#endif // GITFSBACKUPJOBCONFIGURATION_H
