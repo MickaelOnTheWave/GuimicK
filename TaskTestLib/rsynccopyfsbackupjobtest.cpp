6c0173db56b56aeb00c4cd136041e32f32bc14c7
TaskManager <task@manager.com>
2017-05-28 17:33:58 -0300

    Automated backup

---
+++ TaskTestLib/rsynccopyfsbackupjobtest.cpp
@@ -0,0 +1,5 @@
+#include "rsynccopyfsbackupjobtest.h"
+
+RsyncCopyFsBackupJobTest::RsyncCopyFsBackupJobTest()
+{
+}
---
+++ TaskTestLib/rsynccopyfsbackupjobtest.h
@@ -0,0 +1,10 @@
+#ifndef RSYNCCOPYFSBACKUPJOBTEST_H
+#define RSYNCCOPYFSBACKUPJOBTEST_H
+
+class RsyncCopyFsBackupJobTest
+{
+public:
+    RsyncCopyFsBackupJobTest();
+};
+
+#endif // RSYNCCOPYFSBACKUPJOBTEST_H
