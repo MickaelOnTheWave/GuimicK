57be487c31051001fd72e5e8cb8cb692b5d8e491
TaskManager <task@manager.com>
2017-06-18 19:05:25 -0300

    Updated version number

--- TaskTool/main.cpp
+++ TaskTool/main.cpp
@@ -13,7 +13,7 @@
 
 using namespace std;
 
-static const string PROGRAM_VERSION = "0.614";
+static const string PROGRAM_VERSION = "0.615";
 static const string DEFAULT_CONFIGURATION_FILE = "configuration.txt";
 
 void ShowErrors(vector<string> &errorMessages);
