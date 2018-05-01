b7ee6c8658d3c3fd0d3ee1504df6ccc232631617
TaskManager <task@manager.com>
2018-05-01 14:53:05 -0300

    Improved partial static build script

--- TaskTool/MakeLinuxParticalStaticBuild.sh
+++ TaskTool/MakeLinuxParticalStaticBuild.sh
@@ -3,20 +3,14 @@
 TARGET_DIRECTORY="/home/mickael/Prog/TaskManager/bin/PartialStatic/"
 FINAL_LINK_PATH=$TARGET_DIRECTORY"FinalLink"
 
-TOOLS_BUILD_PATH="/home/mickael/Prog/Tools/bin/Synology/"
-TOOLSLIB_BIN_PATH=$TOOLS_BUILD_PATH"ToolsLib/"
 TOOLSLIB_PATH="/home/mickael/Prog/Tools/ToolsLib/"
 
 TASKMANAGER_BUILD_PATH="/home/mickael/Prog/TaskManager/bin/Synology/"
 
-PARSERSLIB_BIN_PATH1="/home/mickael/Prog/TaskManager/bin/Synology/ReportParsers/"
-PARSERSLIB_BIN_PATH=$PARSERSLIB_BIN_PATH1"ParsersLib/"
 PARSERSLIB_PATH="/home/mickael/Prog/TaskManager/ReportParsers/ParsersLib/"
 
-TASKLIB_BIN_PATH="/home/mickael/Prog/TaskManager/bin/Synology/TaskLib"
 TASKLIB_PATH="/home/mickael/Prog/TaskManager/TaskLib/"
 
-TASKTOOL_BIN_PATH="/home/mickael/Prog/TaskManager/bin/Synology/TaskTool"
 TASKTOOL_PATH="/home/mickael/Prog/TaskManager/TaskTool/"
 
 
@@ -42,27 +36,44 @@ buildModule()
 	make -f $MAKEFILE
 }
 
+buildTaskModule()
+{
+	NAME="TaskLib"
+	LIBPATH=$TASKLIB_PATH
+	BINPATH=$TARGET_DIRECTORY$NAME
+	MAKEFILE=$NAME"Makefile"
+	
+	createFolderIfNotPresent $TARGET_DIRECTORY
+	createFolderIfNotPresent $BINPATH
+	cd $BINPATH
+	
+	TOOLSBINPATH=$TARGET_DIRECTORY"ToolsLib"
+	PARSERSBINPATH=$TARGET_DIRECTORY"ParsersLib"
+	qmake -o $MAKEFILE "LIBS += -L$TOOLSBINPATH -L$PARSERSBINPATH" $LIBPATH/$NAME".pro"
+	make -f $MAKEFILE
+}
+
 extractModule()
 {
 	NAME=$1
 	PATH=$TARGET_DIRECTORY$NAME"/static"
-	cp $PATH"/*.o ./"
+	/bin/cp $PATH/*.o ./
 }
 
 linkEverything()
 {
 	createFolderIfNotPresent $FINAL_LINK_PATH
 	cd $FINAL_LINK_PATH
-	#extractModule "ToolsLib"
-	#extractModule "ParsersLib"
-	#extractModule "TaskLib"
-	ar -c libTaskManager.a *.o	
+	extractModule "ToolsLib"
+	extractModule "ParsersLib"
+	extractModule "TaskLib"
+	/usr/bin/ar -c libTaskManager.a *.o	
 }
 
 buildModule "ToolsLib" $TOOLSLIB_PATH
 
 buildModule "ParsersLib" $PARSERSLIB_PATH
 
-buildModule "TaskLib" $TASKLIB_PATH
+buildTaskModule
 
 linkEverything
