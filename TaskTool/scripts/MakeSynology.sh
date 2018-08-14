929d9548a42a8b5d1bd50cf50612534b0298a48d
TaskManager <task@manager.com>
2018-08-13 21:48:49 -0300

    Finished Synology cross build script. Updated version number.

--- TaskTool/scripts/MakeSynology.sh
+++ TaskTool/scripts/MakeSynology.sh
@@ -3,5 +3,5 @@
 source BuildTools.sh
 
 prepareForBuild "Synology"
-cmake ../../proj/cmake -DCMAKE_BUILD_TYPE=RELEASE -DUSE_OLD_COMPILER=TRUE
+cmake ../../proj/cmake -DCMAKE_BUILD_TYPE=RELEASE -DUSE_OLD_COMPILER=TRUE -DCMAKE_TOOLCHAIN_FILE=/home/mickael/Prog/TaskManager/TaskTool/scripts/synology.cmake
 build
