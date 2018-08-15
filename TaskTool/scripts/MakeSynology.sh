7fe2d946dcb9f6506c9370f61a452fe2eb9f8f36
TaskManager <task@manager.com>
2018-08-15 19:52:18 -0300

    Updated Release plan. Cleaned Synology build script.

--- TaskTool/scripts/MakeSynology.sh
+++ TaskTool/scripts/MakeSynology.sh
@@ -2,6 +2,8 @@
 
 source BuildTools.sh
 
+SCRIPTDIR=$PWD
+
 prepareForBuild "Synology"
-cmake ../../proj/cmake -DCMAKE_BUILD_TYPE=RELEASE -DUSE_OLD_COMPILER=TRUE -DCMAKE_TOOLCHAIN_FILE=/home/mickael/Prog/TaskManager/TaskTool/scripts/synology.cmake
+cmake ../../proj/cmake -DCMAKE_BUILD_TYPE=RELEASE -DUSE_OLD_COMPILER=TRUE -DCMAKE_TOOLCHAIN_FILE=$SCRIPTDIR/synology.cmake
 build
