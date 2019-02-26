a9e9ac736a3f40e7efbabc80e5860ddebf90e9a9
Mickaël C. Guimaraes (Windows) <mickael.dacruz@gmail.com>
2019-02-26 15:50:49 -0800

    Added Debug build script for Editor. Removed source_group from Cmake files.

---
+++ ConfigurationEditingTool/scripts/MakeWin64VsDebug.bat
@@ -0,0 +1,15 @@
+
+set buildname="Win64Vs"
+
+IF NOT EXIST ..\bin (
+	mkdir ..\bin
+)
+cd ..\bin
+
+IF NOT EXIST %buildname% (
+	mkdir %buildname%
+)
+cd %buildname%
+
+cmake ../../proj/cmake -G "Visual Studio 15 2017 Win64"
+cmake --build . --config Debug -j 8
