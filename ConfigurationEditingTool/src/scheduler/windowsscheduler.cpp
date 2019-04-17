c5e038015f15e83aed4e523ecdd2deae4f7c4b02
Mickaël C. Guimaraes (Windows) <mickael.dacruz@gmail.com>
2019-04-16 18:22:24 -0700

    Started implementing Windows system scheduler API

--- ConfigurationEditingTool/src/scheduler/windowsscheduler.cpp
+++ ConfigurationEditingTool/src/scheduler/windowsscheduler.cpp
@@ -1,16 +1,78 @@
 #include "windowsscheduler.h"
 
+#include <comdef.h>
+
 WindowsScheduler::WindowsScheduler()
 {
+   InitializeWindowsApi();
+}
 
+WindowsScheduler::~WindowsScheduler()
+{
+   if (taskServiceAvailable)
+      taskService->Release();
+
+   if (comInitialized)
+      CoUninitialize();
 }
 
 bool WindowsScheduler::Read(ScheduleTarget& data)
 {
-    return false;
+   if (comInitialized && winApiAvailable)
+   {
+      return false;
+   }
+   else
+      return false;
 }
 
 bool WindowsScheduler::Write(const ScheduleTarget& data)
 {
-    return false;
+   if (comInitialized && winApiAvailable)
+   {
+      return false;
+   }
+   else
+      return false;
+}
+
+void WindowsScheduler::InitializeWindowsApi()
+{
+   // TODO : finish copying from docs.microsoft.com : weekly trigger example
+   InitializeCom();
+   InitializeComSecurity();
+   CreateTaskService();
+   //CreateTaskFolder();
+   //DefineTaskInformation();
+   //CreateTrigger();
+   //CreateTaskAction();
+}
+
+void WindowsScheduler::InitializeCom()
+{
+   HRESULT hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
+   comInitialized = SUCCEEDED(hr);
+}
+
+void WindowsScheduler::InitializeComSecurity()
+{
+   HRESULT hr = CoInitializeSecurity(NULL, -1, NULL, NULL, 
+                                     RPC_C_AUTHN_LEVEL_PKT,
+                                     RPC_C_IMP_LEVEL_IMPERSONATE,
+                                     NULL, 0, NULL);
+   winApiAvailable = SUCCEEDED(hr);
+}
+
+void WindowsScheduler::CreateTaskService()
+{
+   HRESULT hr = CoCreateInstance(CLSID_TaskScheduler, NULL,
+                                 CLSCTX_INPROC_SERVER,
+                                 IID_ITaskService, (void**)&taskService);
+   if (SUCCEEDED(hr))
+   {
+      hr = taskService->Connect(_variant_t(), _variant_t(), _variant_t(), _variant_t());
+      taskServiceAvailable = SUCCEEDED(hr);
+   }
+
+   winApiAvailable = taskServiceAvailable;
 }
--- ConfigurationEditingTool/src/scheduler/windowsscheduler.h
+++ ConfigurationEditingTool/src/scheduler/windowsscheduler.h
@@ -3,13 +3,27 @@
 
 #include "abstractscheduler.h"
 
+#include <taskschd.h>
+
 class WindowsScheduler : public AbstractScheduler
 {
 public:
    WindowsScheduler();
+   ~WindowsScheduler();
 
    bool Read(ScheduleTarget &data) override;
    bool Write(const ScheduleTarget &data) override;
+
+private:
+   void InitializeWindowsApi();
+   void InitializeCom();
+   void InitializeComSecurity();
+   void CreateTaskService();
+
+   bool comInitialized = false;
+   bool winApiAvailable = true;
+   bool taskServiceAvailable = true;
+   ITaskService* taskService = nullptr;
 };
 
 #endif // WINDOWSSCHEDULER_H
