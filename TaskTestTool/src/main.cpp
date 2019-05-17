098f7a826991353bc1bb49bf0f02688469929258
Mickaël C. Guimarães <mickael.dacruz@gmail.com>
2019-05-16 22:54:57 -0300

    Resolved compilation issues with TaskTestTool

--- TaskTestTool/src/main.cpp
+++ TaskTestTool/src/main.cpp
@@ -3,7 +3,9 @@
 
 #include "commandlinemanager.h"
 #include "filetools.h"
+#include "pathtools.h"
 #include "qtbatchtestrunner.h"
+#include "stringtools.h"
 
 #include "parserstestlib.h"
 #include "qttoolstestlib.h"
@@ -14,14 +16,14 @@ using namespace std;
 
 // @TODO : Make these folders relative and update Cmake to have an install target
 // that copies test data to binary dir.
-const string progDir = "/home/mickael/Prog";
-const string taskManagerDir = progDir + "/TaskManager";
-const string taskLibDir  = taskManagerDir + "/TaskTestLib/data/";
-const string parsersDir  = taskManagerDir + "/ReportParsers/ParsersTestLib/data/";
-const string toolsLibDir = progDir + "/Tools/ToolsTestLib/data/";
-const string errorDir    = taskManagerDir + "/TaskTestLib/data/errors/";
+const wstring progDir = L"/home/mickael/Prog";
+const wstring taskManagerDir = progDir + L"/TaskManager";
+const wstring taskLibDir  = taskManagerDir + L"/TaskTestLib/data/";
+const wstring parsersDir  = taskManagerDir + L"/ReportParsers/ParsersTestLib/data/";
+const wstring toolsLibDir = progDir + L"/Tools/ToolsTestLib/data/";
+const wstring errorDir    = taskManagerDir + L"/TaskTestLib/data/errors/";
 
-const string testingFolder = "TaskManagerTestSuites/";
+const wstring testingFolder = L"TaskManagerTestSuites/";
 
 int main(int argc, char* argv[])
 {
@@ -32,16 +34,16 @@ int main(int argc, char* argv[])
     const int ERRORS = 1;
     const int DID_NOT_RUN = 2;
 
-    int result = chdir(testingFolder.c_str());
-    if (result != 0)
+    const bool ok = PathTools::ChangeCurrentDir(testingFolder);
+    if (!ok)
     {
-       cout << "Can't chdir to folder " << testingFolder << endl;
+       wcout << "Can't chdir to folder " << testingFolder << endl;
        return DID_NOT_RUN;
     }
 
     CommandLineManager commandLine(argc, argv);
-    commandLine.AddParameter("silent", "Runs in silent mode, no output on console.");
-    commandLine.AddParameter("detailed", "Displays detailed results.");
+    commandLine.AddParameter(L"silent", L"Runs in silent mode, no output on console.");
+    commandLine.AddParameter(L"detailed", L"Displays detailed results.");
     commandLine.EnableHelpCommand();
 
     if (commandLine.HandleUnknownParameters())
@@ -58,12 +60,12 @@ int main(int argc, char* argv[])
     GetToolsLibTests(tests, toolsLibDir);
 
     QtBatchTestRunner runner(argv[0]);
-    runner.SetTempResultFile(errorDir + "result.xml");
+    runner.SetTempResultFile(StringTools::UnicodeToUtf8(errorDir + L"result.xml"));
     runner.Run(tests);
 
-    if (commandLine.HasParameter("silent") == false)
+    if (commandLine.HasParameter(L"silent") == false)
     {
-        if (commandLine.HasParameter("detailed"))
+        if (commandLine.HasParameter(L"detailed"))
             runner.ShowFullData();
         else
             runner.ShowSummarizedData();
