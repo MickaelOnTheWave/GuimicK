c8d89cfff1712eedab717b6c7eac8aeac20058b8
Mickaël C. Guimaraes (Windows) <mickael.dacruz@gmail.com>
2019-05-20 14:44:47 -0700

    Fixed Cmake for Ninja generator. Removed obsolete build script.

--- TaskTool/scripts/MakeNativeWindowsRelease.bat
+++
@@ -1,8 +0,0 @@
-
-cd ../bin
-IF NOT EXIST WindowsRelease (
-	mkdir WindowsRelease
-)
-cd ../bin/WindowsRelease
-
-cmake ../../proj/cmake -DCMAKE_BUILD_TYPE=RELEASE -DUSE_CURL_LIB=FALSE -DCMAKE_TOOLCHAIN_FILE=../../scripts/winNative.cmake
