e584922e4c39a2d701c50baf9d1a97346066b3e5
Mickaël C. Guimaraes (Windows) <mickael.dacruz@gmail.com>
2019-11-18 13:29:23 -0800

    Created TaskTool envelope app.

--- TaskToolLauncher/src/main.cpp
+++ TaskToolLauncher/src/main.cpp
@@ -1,9 +1,25 @@
-#include <iostream>
+#include <sstream>
+
+#include "filetools.h"
+#include "tools.h"
 
 using namespace std;
 
+const wstring returnCodeFile = L"taskToolReturnCode";
+const std::wstring outputFile = L"taskToolOutput";
+const std::wstring commandFile = L"taskToolCommand";
+
+std::wstring ToWstring(const int value)
+{
+   wstringstream stream;
+   stream << value;
+   return stream.str();
+}
+
 int main(void)
 {
-    cout << "Hello World! :-)" << endl;
-    return 0;
+   const std::wstring commandToRun = FileTools::GetTextFileContent(commandFile);
+   const int returnCode = Tools::RunExternalCommandToFile(commandToRun, outputFile);
+   FileTools::WriteBufferToFile(returnCodeFile, ToWstring(returnCode));
+   return 0;
 }
