dac7b52a55082974f61d7c87a94c500b15a22002
Mickaël C. Guimarães <mickael.dacruz@gmail.com>
2020-02-12 13:09:06 -0300

    Added forgotten file

---
+++ TaskLib/include/emailaccountdataconfiguration.h
@@ -0,0 +1,26 @@
+#ifndef EMAILACCOUNTDATACONFIGURATION_H
+#define EMAILACCOUNTDATACONFIGURATION_H
+
+#include "configurationobject.h"
+#include "emailaccountdata.h"
+
+class EmailAccountDataConfiguration
+{
+public:
+   EmailAccountDataConfiguration(EmailAccountData& _dataObject);
+
+   std::wstring GetName() const;
+
+   void Load(ConfigurationObject *confObject,
+             std::vector<std::wstring> &errorMessages);
+
+   void SaveToFile(std::wofstream& fileStream);
+
+private:
+   void LoadProperty(const std::pair<std::wstring, std::wstring>& property,
+                     std::vector<std::wstring> &errorMessages);
+
+   EmailAccountData& dataObject;
+};
+
+#endif // EMAILACCOUNTDATACONFIGURATION_H
